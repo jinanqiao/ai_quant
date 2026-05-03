dd4ca6bfe486a2723ffa2f4642793b3371c4874f
jinanqiao <jinanqiao@users.noreply.github.com>
2026-05-03 15:40:08 +0800

    学习参数设置

---
+++ DeltaF_AI_Python_Quant/14_Your_First_Strategy/testTranslation.py
@@ -0,0 +1,4 @@
+# Hello world, this is a translation test from Cursor
+greeting_message = "Welcome to the quantitative trading workshop"
+account_balance = 100000
+# Strategy: buy when the moving average crosses upward
